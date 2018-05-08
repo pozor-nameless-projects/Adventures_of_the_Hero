b777d5a52d2df498e1fd519554d0fa17a2e517cb
Danil <danil.present@mail.ru>
2018-05-08 23:46:39 +0800

    Add dialogs

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import time
 import os
 
 
-user = ''
-age = ''
+user = 'Потохер'
+age = '56'
 
 
 screen_saver_thread = None
@@ -38,18 +38,22 @@ class hero:
 
 
 def printkrasivo(s):
+	s = s + '\n'
 	strtoprint = ''
 	for i in s:
 		strtoprint = strtoprint + str(i)
 		sys.stdout.write('\r\r' + (strtoprint) + '')
-		time.sleep(0.01)
+		if i != ' ':
+			time.sleep(0.05)
+		else:
+			pass
 
 
 #heroes
-gnom = hero('Гном', 120, 'Гномы обитают в подземелье. Они не опасны, пока их не разозлить', 5, 0)
-dragon = hero('Дракон', 562, 'Дракон очень сверепое существо. Остерегайтесь его в замке', 14, 1)
-elf = hero('Эльф', 332, 'Эльфы стреляют своими магическими стрелами.', 10, 1)
-trader = hero('Торговец', 24, 'Торгует всем что есть в это мире.', 1, 0)
+gnom = hero('Гном', random.randint(60, 140), 'Гномы обитают в подземелье. Они не опасны, пока их не разозлить', 5, 0)
+dragon = hero('Дракон', random.randint(400, 600), 'Дракон очень сверепое существо. Остерегайтесь его в замке', 14, 1)
+elf = hero('Эльф', random.randint(120, 340), 'Эльфы стреляют своими магическими стрелами.', 10, 1)
+trader = hero('Торговец', random.randint(40, 100), 'Торгует всем что есть в это мире.', 1, 0)
 villager = hero('Житель', random.randint(20, 40), 'Простой житель. Ничего не умеет. Ничего не делает.', 0, 0)
 old_villager = hero('Старый житель', random.randint(50, 85), 'Старый житель. Живет в избушке на пенсии', 0, 0)
 young_villager = hero('Молодой житель', random.randint(10, 18), 'Молодой житель. Еще учится в школе.', 0, 0)
@@ -59,98 +63,96 @@ torch = item('Факел', 'Факел для освещения дороги.',
 small_sword = item('Маленький меч', 'Простой меч. Наносит мало урона.', 3, 100)
 
 
+
 # h = people(user, age, 'Ваш перссонаж бродит по темным уголкам этого средневекового века. Он ищет не только приключения, но и семь тоинственных душ')
-class windows:
-	global user
-	global age
-	menu = None
-	create_person = None
+menu = None
+create_person = None
 
 
-	def menu(self):
-		global menu
-		menu = Tk()
+def menu():
+	global menu
+	menu = Tk()
 
-		menu.title('Adventures of the Hero')
-		menu.geometry('450x450')
-		menu.config(bg = '#1FA7E1')
-		menu.resizable(0, 0)
+	menu.title('Adventures of the Hero')
+	menu.geometry('450x450')
+	menu.config(bg = '#1FA7E1')
+	menu.resizable(0, 0)
 
-		label = Label(menu, text = 'Menu', bg='#1FA7E1', fg='white')
-		label.config(font = ('Arial', 25, 'bold'))
-		label.place(x=180, y=30)
-		
-		button = Button(menu, text = 'START', width=15, command=self.create_person)
-		button.config(font = ('Arial', 15, 'bold'))
-		button.place(x=130, y=100)
+	label = Label(menu, text = 'Menu', bg='#1FA7E1', fg='white')
+	label.config(font = ('Arial', 25, 'bold'))
+	label.place(x=180, y=30)
+	
+	button = Button(menu, text = 'START', width=15, command=create_person)
+	button.config(font = ('Arial', 15, 'bold'))
+	button.place(x=130, y=100)
 
-		button = Button(menu, text = 'CONTINUE', width=15, command=self.create_person)
-		button.config(font = ('Arial', 15, 'bold'))
-		button.place(x=130, y=170)
+	button = Button(menu, text = 'CONTINUE', width=15, command=create_person)
+	button.config(font = ('Arial', 15, 'bold'))
+	button.place(x=130, y=170)
 
-		button = Button(menu, text = 'SETTINGS', width=15)
-		button.config(font = ('Arial', 15, 'bold'))
-		button.place(x=130, y=240)
+	button = Button(menu, text = 'SETTINGS', width=15)
+	button.config(font = ('Arial', 15, 'bold'))
+	button.place(x=130, y=240)
 
-		button = Button(menu, text = 'EXIT', width=15, command=menu.destroy)
-		button.config(font = ('Arial', 15, 'bold'))
-		button.place(x=130, y=310)
+	button = Button(menu, text = 'EXIT', width=15, command=menu.destroy)
+	button.config(font = ('Arial', 15, 'bold'))
+	button.place(x=130, y=310)
 
-		menu.mainloop()
-		
-	 
-	def create_person(self):
-		global create_person
+	menu.mainloop()
+	
+ 
+def create_person():
+	global create_person
 
-		def con():
-			set_user_data(entry, entry1)
-			game_windows.game()
+	def con():
+		set_user_data(entry, entry1)
+		game()
 
-		menu.destroy()
-		create_person = Tk()
-		create_person.resizable(0, 0)
+	menu.destroy()
+	create_person = Tk()
+	create_person.resizable(0, 0)
 
-		create_person.title('Adventures of the Hero')
-		create_person.geometry('450x450')
-		create_person.config(bg = '#1FA7E1')
+	create_person.title('Adventures of the Hero')
+	create_person.geometry('450x450')
+	create_person.config(bg = '#1FA7E1')
 
-		label = Label(create_person, text = 'Input name: ', bg='#1FA7E1', fg='white')
-		label.config(font = ('Arial', 20, 'bold'))
-		label.place(x=130, y=30)
+	label = Label(create_person, text = 'Input name: ', bg='#1FA7E1', fg='white')
+	label.config(font = ('Arial', 20, 'bold'))
+	label.place(x=130, y=30)
 
-		entry = Entry(create_person, width = 20, )
-		entry.config(font = ('Arial', 15, 'bold'))
-		entry.place(x=130, y=70)
+	entry = Entry(create_person, width = 20, )
+	entry.config(font = ('Arial', 15, 'bold'))
+	entry.place(x=130, y=70)
 
-		label = Label(create_person, text = 'Input Age: ', bg='#1FA7E1', fg='white')
-		label.config(font = ('Arial', 20, 'bold'))
-		label.place(x=130, y=110)
+	label = Label(create_person, text = 'Input Age: ', bg='#1FA7E1', fg='white')
+	label.config(font = ('Arial', 20, 'bold'))
+	label.place(x=130, y=110)
 
-		entry1 = Entry(create_person, width = 20)
-		entry1.config(font = ('Arial', 15, 'bold'))
-		entry1.place(x=130, y=150)
+	entry1 = Entry(create_person, width = 20)
+	entry1.config(font = ('Arial', 15, 'bold'))
+	entry1.place(x=130, y=150)
 
-		button = Button(create_person, text = 'Create person', width=15, command=con)
-		button.config(font = ('Arial', 15, 'bold'))
-		button.place(x=130, y=190)
+	button = Button(create_person, text = 'Create person', width=15, command=con)
+	button.config(font = ('Arial', 15, 'bold'))
+	button.place(x=130, y=190)
 
-		create_person.mainloop()
-		try:
-			screen_saver_thread.join()
-		except:
-			pass
+	create_person.mainloop()
+	try:
+		screen_saver_thread.join()
+	except:
+		pass
 
-	def inv(self):
-		global inv
-		
-		inv = Tk()
+def inv():
+	global inv
+	
+	inv = Tk()
 
-		inv.title('Adventures of the Hero')
-		inv.geometry('450x450')
-		inv.config(bg = '#1FA7E1')
-		inv.resizable(0, 0)
+	inv.title('Adventures of the Hero')
+	inv.geometry('450x450')
+	inv.config(bg = '#1FA7E1')
+	inv.resizable(0, 0)
 
-		game.mainloop()
+	game.mainloop()
 
 
 def set_user_data(entry1, entry2):
@@ -193,16 +195,82 @@ def screen_saver():
 
 	for i in strs:
 		printkrasivo(i + '\n')
-                                                                                 
+																				 
 
 def game():
-	pass
+	# create_person.destroy()
+	# screen_saver_thread.join()
+	stage = 0
+	if stage == 0:
+		p('-' + villager.name)
+		printkrasivo('Здравствуй, ' + user + ', какими судьбами тебя занесло в наши края?')
+		p('')
+		p('1.Я пришел в ваши края за семи магическими душами, разных существ.')
+		p('2.Пришел найти своего друга - гнома Нерута. Ты случайно не знаешь его?')
+		p('3.Я думаю тебе не обязательно знать это!')
+		v1 = input('>>>')
+		if v1 == '1':
+			cls()
+			p('-' + villager.name)
+			p('')
+		if v1 == '2':
+			cls()
+			p('-' + villager.name)
+			printkrasivo('Да, Я слышал о нем немного. Говорят он работает местным шахтером.')
+			p('')
+			p('1.Мог бы ты меня провести до шахты?')
+			p('2.Подскажи пожалуйста мне дорогу.')
+			p('')
+			v2 = input('>>>')
+			if v2 == '1':
+				cls()
+				p('-' + villager.name)
+				printkrasivo('Да, конечно, но это будет стоить 15 монет.')
+				p('')
+				p('1.Хорошо, пошли!')
+				p('2.Нет, спасибо! Подскажи мне пожайлуйста дорогу')
+				v3 = input('>>>')
+				if v3 == '1':
+					cls()
+					printkrasivo('- > - > - > - > - > - > - > - > - > - >')
+					cls()
+					p('-' + user)
+					printkrasivo('Стой! Мне надо зайти в торговую лавку. Подожди меня тут.')
+					time.sleep(1)
+					p('')
+					p('-' + villager.name)
+					printkrasivo('Хорошо я жду!')
+				if v3 == '2':
+					cls()
+					p('-' + villager.name)
+					printkrasivo('Иди прямо до деревни. Проходи мимо торговой лавки и там увидешь водопад. Под ним и будет его шахта!')
+					p('')
+					p('-' + user)
+					printkrasivo('Спасибо!')
+					time.sleep(5)
+					cls()
+					printkrasivo('- > - > - > - > - > - > - > - > - > - >')
+					cls()
+			if v2 == '2':
+				printkrasivo('Иди прямо до деревни. Проходи мимо торговой лавки и там увидешь водопад. Под ним и будет его шахта!')
+				p('')
+				p('-' + user)
+				printkrasivo('Спасибо!')
+				time.sleep(5)
+				cls()
+				printkrasivo('- > - > - > - > - > - > - > - > - > - >')
+				cls()
+
+		if v1 == '3':
+			cls()
+			p('-' + villager.name)
+			printkrasivo('Хорошо, Я просто хотел предложить тебе подзаработать немного денег. Я думаю в наших краях они будут полезны! ')
+			p('')
 
 
 if __name__ == '__main__':
 	cls()
-	screen_saver_thread = threading.Thread(target = screen_saver)
-	screen_saver_thread.start()
-	game_windows = windows()
-	game_windows.menu()
-
+	# screen_saver_thread = threading.Thread(target = screen_saver)
+	# screen_saver_thread.start()
+	# menu()
+	game()
