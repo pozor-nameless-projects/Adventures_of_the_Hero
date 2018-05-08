022a4f65e2305cf3651f5821809b020e88d4e455
Dmitriy <stdian@mail.ru>
2018-05-08 17:12:01 +0800

    Added weapons

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class item:
 
 class hero:
 
-
 	def __init__(self, name, age, des, dam, den):
 		self.name = name
 		self.age = age
@@ -27,15 +26,19 @@ class hero:
 		self.dam = dam
 		self.den = den
 
+
+#heroes
 gnom = hero('Gnom', 120, 'Гномы обитают в подземелье. Они не опасны, пока их не разозлить', 5, 0)
 dragon = hero('Dragon', 562, 'Дракон очень сверепое существо. Остерегайтесь его в замке', 14, 1)
 elf = hero('Elf', 332, 'Эльфы стреляют своими магическими стрелами.', 10, 1)
 trader = hero('Trader', 24, 'Торгует всем что есть в это мире.', 1, 0)
 villager = hero('Villager', random.randint(20, 40), 'Простой житель. Ничего не умеет. Ничего не делает.', 0, 0)
 old_villager = hero('Old villager', random.randint(50, 85), 'Старый житель. Живет в избушке на пенсии', 0, 0)
-
 young_villager = hero('Young villager', random.randint(10, 18), 'Молодой житель. Еще учится в школе.', 0, 0)
- 
+#items
+wooden_stick = item('Палка', 'Палка из дерева. Проще её сжечь чем кого-нибудь ей убить', 1, 15)
+torch = item('Факел', 'Факел для освещения дороги.', 2, 35)
+small_sword = item('Маленький меч', 'Простой меч. Наносит мало урона.', 3, 100)
 
 
 # h = people(user, age, 'Ваш перссонаж бродит по темным уголкам этого средневекового века. Он ищет не только приключения, но и семь тоинственных душ')
